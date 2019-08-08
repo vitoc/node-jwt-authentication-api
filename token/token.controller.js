94deb73f63da18b28d8e9610d6caa08d33849dd6
Vito Chin <vito@php.net>
2019-08-08 11:26:45 +0800

    Added frontendUrl as config

--- token/token.controller.js
+++ token/token.controller.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const LocalStorage = require('node-localstorage').LocalStorage;
 const localStorage = new LocalStorage('./tokens');
-
+const config = require('config.json');
 // routes
 router.post('/', receive);
 
 module.exports = router;
 
 function receive(req, res, next) {
+    const { frontendUrl } = config;
     localStorage.setItem(req.body.state, req.body.id_token);
-    res.redirect('http://localhost:8080/login');
+    res.redirect(frontendUrl);
 }
